1b6f28d559a42d48c507b0631d5cee52d45c74fb
Takumikimura141899 <kimura141899@gmail.com>
2021-08-14 15:06:30 +0900

    SwitchCountry

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import './App.css';
 import Cards from './features/covid/Cards/Cards';
 import Chart from './features/covid/Chart/Chart';
+import SwitchCountry from './features/covid/Chart/PieChart/DashBoard/SwitchCoutry/SwitchCountry';
+import PieChart from './features/covid/Chart/PieChart/PieChart';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <header className='App-header'>
         <Cards />
         <Chart />
+        <PieChart />
+        <SwitchCountry />
       </header>
     </div>
   );
